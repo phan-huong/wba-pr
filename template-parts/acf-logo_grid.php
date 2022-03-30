419845d1972dd3a0c21cbe39bd4776eb220beb46
Phan Hương <phanngochuong@hotmail.com>
2022-03-30 14:07:43 +0200

    Add section Logo Grid

---
+++ template-parts/acf-logo_grid.php
@@ -0,0 +1,24 @@
+<?php if( get_row_layout() == 'logo_grid' ) : ?>
+    <section class="logo_grid">
+        <div class="logo_grid__first_row">
+            <?php if( have_rows('logos_first_row') ) : 
+                while ( have_rows('logos_first_row') ) : the_row(); 
+                    $logo = get_sub_field('logo'); ?>
+
+                    <img src="<?php echo $logo ?>">
+
+                <?php endwhile;
+            endif; ?>
+        </div>
+        <div class="logo_grid__second_row">
+            <?php if( have_rows('logos_second_row') ) : 
+                while ( have_rows('logos_second_row') ) : the_row(); 
+                    $logo = get_sub_field('logo'); ?>
+
+                    <img src="<?php echo $logo ?>">
+
+                <?php endwhile;
+            endif; ?>
+        </div>
+    </section>
+<?php endif; ?>
